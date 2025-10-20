50278f035ef4a7a52882240e061e342b69688c08
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-10-20 23:19:47 +0200

    feat: Added Convex and finished user addition setup

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import {
+  LogoutLink,
+  useKindeBrowserClient,
+} from "@kinde-oss/kinde-auth-nextjs";
+import { useConvex, useMutation } from "convex/react";
+import React, { useEffect, useRef } from "react";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const convex = useConvex();
+  const { user } = useKindeBrowserClient();
+  const createUser = useMutation(api.user.createUser);
 
-export default Dashboard
+  const hasCreated = useRef(false);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      if (!user || hasCreated.current) return;
+
+      try {
+        const result = await convex.query(api.user.getUser, {
+          email: user.email!,
+        });
+
+        if (!result || result.length === 0) {
+          await createUser({
+            name: user.given_name! + " " + user.family_name,
+            email: user.email!,
+            image: user.picture!,
+          });
+        }
+
+        hasCreated.current = true;
+      } catch (err) {
+        console.error("Error checking user:", err);
+      }
+    };
+
+    checkUser();
+  }, [user, convex, createUser]);
+
+  return (
+    <div>
+      Dashboard
+      <LogoutLink>
+        <Button>LogOut</Button>
+      </LogoutLink>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ convex/user.tsx
@@ -0,0 +1,29 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const getUser = query({
+  args: {
+    email: v.string(),
+  },
+
+  handler: async (ctx, args_0) => {
+    const result = await ctx.db
+      .query("user")
+      .filter((q) => q.eq(q.field("email"), args_0.email))
+      .first();
+
+    return result ?? null;
+  },
+});
+
+export const createUser = mutation({
+  args: {
+    name: v.string(),
+    email: v.string(),
+    image: v.string(),
+  },
+
+  handler: async (ctx, args) => {
+    return await ctx.db.insert("user", args);
+  },
+});
