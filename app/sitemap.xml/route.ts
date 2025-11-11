fc8b86891a744a0adb7569a7dcc1ca83c424d0c5
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-11-11 22:47:45 +0200

    Added more pages reference to my sitemap

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -5,6 +5,17 @@ export async function GET() {
 
   const urls = [
     { loc: `${baseUrl}/`, priority: 1.0 },
+
+    { loc: `${baseUrl}/signin`, priority: 0.8 },
+    { loc: `${baseUrl}/signup`, priority: 0.8 },
+
+    { loc: `${baseUrl}/support/contact`, priority: 0.6 },
+    { loc: `${baseUrl}/legal/about-us`, priority: 0.6 },
+    { loc: `${baseUrl}/support/help-center`, priority: 0.6 },
+
+    { loc: `${baseUrl}/legal/community-guidelines`, priority: 0.4 },
+    { loc: `${baseUrl}/legal/privacy-policy`, priority: 0.4 },
+    { loc: `${baseUrl}/legal/terms-and-conditions`, priority: 0.4 },
   ];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
