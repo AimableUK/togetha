f998d58be72dce0490cf42827f33dbb769a22083
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-11-13 13:31:07 +0200

    fix: Changed fileId Type

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -1,13 +1,14 @@
 import { pageMetadata } from "@/lib/utils";
 import WorkspaceClient from "./WorkspaceClient";
 import React from "react";
+import { Id } from "@/convex/_generated/dataModel";
 
 export const metadata = pageMetadata.workspace;
 
 export default function Workspace({
   params,
 }: {
-  params: Promise<{ fileId: string }>;
+  params: Promise<{ fileId: Id<"files"> }>;
 }) {
   const { fileId } = React.use(params);
   return <WorkspaceClient fileId={fileId} />;
